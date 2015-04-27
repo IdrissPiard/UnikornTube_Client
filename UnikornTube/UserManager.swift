a0c09319ffb8324f3ef28a140398a7ed7e2a745d
Damien Serin <damien@Mac-mini-de-SimpleAndNew1.local>
2015-04-27 18:15:05 +0200

    Add files for signin/signup

---
+++ UnikornTube/UserManager.swift
@@ -0,0 +1,13 @@
+//
+//  UserManager.swift
+//  UnikornTube
+//
+//  Created by Damien Serin on 27/04/2015.
+//  Copyright (c) 2015 SimpleAndNew. All rights reserved.
+//
+
+import UIKit
+
+class UserManager: NSObject {
+   
+}
